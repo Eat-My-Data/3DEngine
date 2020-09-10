57a3520d8f3e21cfc954ee69cbc19258a32eefa9
SleepyEthan <ethan2015d@gmail.com>
2020-09-09 20:25:30 -0500

    Window can now close with custom code.

--- SleepyEngine/SleepyEngine/WinMain.cpp
+++ SleepyEngine/SleepyEngine/WinMain.cpp
@@ -1,5 +1,16 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc( HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam )
+{
+	switch ( msg )
+	{
+	case WM_CLOSE:
+		PostQuitMessage( 1 );
+		break;
+	}
+	return DefWindowProc( hWnd,msg,wParam,lParam );
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -11,7 +22,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof( wc );
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -34,6 +45,23 @@ int CALLBACK WinMain(
 	// show window
 	ShowWindow( hWnd,SW_SHOW );
 
-	while ( true );
-	return 0;
+	// message pump
+	MSG msg;
+	BOOL gResult;
+	while ( (gResult = GetMessage( &msg,nullptr,0,0 )) > 0 )
+	{
+		TranslateMessage( &msg );
+		DispatchMessage( &msg );
+	}
+
+	// -1 for error
+	// msg.wParam is what is passed in the PostQuitMessage function
+	if ( gResult == -1 )
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
