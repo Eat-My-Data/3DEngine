122b31f90f1ed63bde8b81b025517c3da53ee7d2
SleepyEthan <ethan2015d@gmail.com>
2020-11-21 13:42:24 -0600

    Object is now following mouse, using directxmath matrix and it's rotation/scaling/translation.

--- 3DEngine/3DEngine/Graphics.cpp
+++ 3DEngine/3DEngine/Graphics.cpp
@@ -2,8 +2,10 @@
 #include "dxerr.h"
 #include <sstream>
 #include "d3dcompiler.h"
+#include <DirectXMath.h>
 
 namespace wrl = Microsoft::WRL;
+namespace dx = DirectX;
 
 #pragma comment(lib,"d3d11.lib")
 #pragma comment(lib,"D3DCompiler.lib")
@@ -98,7 +100,7 @@ void Graphics::ClearBuffer( float red,float green,float blue ) noexcept
 	pContext->ClearRenderTargetView( pTarget.Get(),color );
 }
 
-void Graphics::DrawTestTriangle( float angle )
+void Graphics::DrawTestTriangle( float angle,float x,float y )
 {
 	namespace wrl = Microsoft::WRL;
 
@@ -177,18 +179,16 @@ void Graphics::DrawTestTriangle( float angle )
 	// create constant buffer for tranformation matrix
 	struct ConstantBuffer
 	{
-		struct 
-		{
-			float element[4][4];
-		} transformation;
+		dx::XMMATRIX transform;
 	};
 	const ConstantBuffer cb =
 	{
 		{
-			( 3.0f / 4.0f ) * std::cos( angle ),	std::sin( angle ),	0.0f,	0.0f,
-			( 3.0f / 4.0f ) * -std::sin( angle ),	std::cos( angle ),	0.0f,	0.0f,
-			0.0f,				0.0f,				1.0f,	0.0f,
-			0.0f,				0.0f,				0.0f,	1.0f,	
+			dx::XMMatrixTranspose(
+				dx::XMMatrixRotationZ( angle ) *
+				dx::XMMatrixScaling( 3.0f / 4.0f,1.0f,1.0f ) * 
+				dx::XMMatrixTranslation( x,y,0.0f )
+			)
 		}
 	};
 	wrl::ComPtr<ID3D11Buffer> pConstantBuffer;
