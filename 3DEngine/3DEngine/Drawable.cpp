66115c303b1e3b33cadf7dd83b3c30da4423c4e1
SleepyEthan <ethan2015d@gmail.com>
2020-11-22 14:40:35 -0600

    New drawable base that Box templates DrawableBase<Box>. This allows us to use the same static binding for numerous bindings.

--- 3DEngine/3DEngine/Drawable.cpp
+++ 3DEngine/3DEngine/Drawable.cpp
@@ -10,12 +10,16 @@ void Drawable::Draw( Graphics& gfx ) const noexcept
 	{
 		b->Bind( gfx );
 	}
+	for ( auto& b : GetStaticBinds() )
+	{
+		b->Bind( gfx );
+	}
 	gfx.DrawIndexed( pIndexBuffer->GetCount() );
 }
 
 void Drawable::AddBind( std::unique_ptr<Bindable> bind ) noexcept
 {
-	assert( "*Must* use AddIndexBuffer to bind index buffer" && typeid( *bind ) != typeid(IndexBuffer) );
+	assert( "*Must* use AddIndexBuffer to bind index buffer" && typeid( *bind ) != typeid( IndexBuffer ) );
 	binds.push_back( std::move( bind ) );
 }
 
--- 3DEngine/3DEngine/Drawable.h
+++ 3DEngine/3DEngine/Drawable.h
@@ -6,16 +6,21 @@ class Bindable;
 
 class Drawable
 {
+	template<class T>
+	friend class DrawableBase;
 public:
 	Drawable() = default;
 	Drawable( const Drawable& ) = delete;
 	virtual DirectX::XMMATRIX GetTransformXM() const noexcept = 0;
 	void Draw( Graphics& gfx ) const noexcept;
 	virtual void Update( float dt ) noexcept = 0;
+	virtual ~Drawable() = default;
+protected:
 	void AddBind( std::unique_ptr<Bindable> bind ) noexcept;
 	void AddIndexBuffer( std::unique_ptr<class IndexBuffer> ibuf ) noexcept;
-	virtual ~Drawable() = default;
 private:
-	const IndexBuffer* pIndexBuffer = nullptr;
+	virtual const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds() const noexcept = 0;
+private:
+	const class IndexBuffer* pIndexBuffer = nullptr;
 	std::vector<std::unique_ptr<Bindable>> binds;
 };
