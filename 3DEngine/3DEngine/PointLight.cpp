9db483d40891ff0f21a3c4e4d0f2a5aebdf3bb04
SleepyEthan <Ethan2015D@gmail.com>
2020-11-26 21:44:47 -0600

    Added Intensity/Color and Fallor to Light gui, made the materialColor independent  by adding in a second constant buffer and adding a bind to the box.

--- 3DEngine/3DEngine/PointLight.cpp
+++ 3DEngine/3DEngine/PointLight.cpp
@@ -17,6 +17,17 @@ void PointLight::SpawnControlWindow() noexcept
 		ImGui::SliderFloat( "X",&cbData.pos.x,-60.0f,60.0f,"%.1f" );
 		ImGui::SliderFloat( "Y",&cbData.pos.y,-60.0f,60.0f,"%.1f" );
 		ImGui::SliderFloat( "Z",&cbData.pos.z,-60.0f,60.0f,"%.1f" );
+		
+		ImGui::Text( "Intensity/Color" );
+		ImGui::SliderFloat( "Intensity",&cbData.diffuseIntensity,0.1f,2.0f,"%.2f" );
+		ImGui::ColorEdit3( "Diffuse Color",&cbData.diffuseColor.x );
+		ImGui::ColorEdit3( "Ambient",&cbData.ambient.x );
+
+		ImGui::Text( "Falloff" );
+		ImGui::SliderFloat( "Constant",&cbData.attConst,0.05f,10.0f,"%.2f" );
+		ImGui::SliderFloat( "Linear",&cbData.attLin, 0.0001f, 4.0f,"%.4f" );
+		ImGui::SliderFloat( "Quadratic",&cbData.attQuad, 0.0000001f, 10.0f,"%.7f" );
+
 		if ( ImGui::Button( "Reset" ) )
 		{
 			Reset();
@@ -29,7 +40,6 @@ void PointLight::Reset() noexcept
 {
 	cbData = {
 		{ 0.0f,0.0f,0.0f },
-		{ 0.7f,0.7f,0.9f },
 		{ 0.05f,0.05f,0.05f },
 		{ 1.0f,1.0f,1.0f },
 		1.0f,
