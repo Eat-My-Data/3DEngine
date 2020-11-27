05c24f990bb62c8c86eaea48e70265bae7fecb35
SleepyEthan <Ethan2015D@gmail.com>
2020-11-27 15:53:35 -0600

    Created test object for common values between drawables, added cylinder, new tesselated indexed triangle list

--- 3DEngine/3DEngine/Cylinder.h
+++ 3DEngine/3DEngine/Cylinder.h
@@ -1,18 +1,14 @@
 #pragma once
 #include "TestObject.h"
 
-class Box : public TestObject<Box>
+class Cylinder : public TestObject<Cylinder>
 {
 public:
-	Box( Graphics& gfx,std::mt19937& rng,
+	Cylinder(Graphics& gfx, std::mt19937& rng,
 		std::uniform_real_distribution<float>& adist,
 		std::uniform_real_distribution<float>& ddist,
 		std::uniform_real_distribution<float>& odist,
 		std::uniform_real_distribution<float>& rdist,
 		std::uniform_real_distribution<float>& bdist,
-		DirectX::XMFLOAT3 material );
-		DirectX::XMMATRIX GetTransformXM() const noexcept;
-private:
-	// model transform
-	DirectX::XMFLOAT3X3 mt;
+		std::uniform_int_distribution<int>& tdist);
 };
--- 3DEngine/3DEngine/PhongPS.hlsl
+++ 3DEngine/3DEngine/PhongPS.hlsl
@@ -22,7 +22,7 @@ float4 main( float3 worldPos : Position,float3 n : Normal ) : SV_TARGET
 	const float3 vToL = lightPos - worldPos;
 	const float distToL = length( vToL );
 	const float3 dirToL = vToL / distToL;
-	// diffuse attenuation
+	// attenuation
 	const float3 att = 1.0f / (attConst + attLin * distToL + attQuad * (distToL * distToL));
 	// diffuse intensity
 	const float3 diffuse = diffuseColor * diffuseIntensity * att * max( 0.0f,dot( dirToL,n ) );
@@ -30,7 +30,7 @@ float4 main( float3 worldPos : Position,float3 n : Normal ) : SV_TARGET
 	const float3 w = n * dot( vToL,n );
     const float3 r = w * 2.0f - vToL;
 	// calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
-    const float3 specular = (diffuseColor * diffuseIntensity) * specularIntensity * pow(max(0.0f, dot(normalize(-r), normalize(worldPos))), specularPower);
+    const float3 specular = att * (diffuseColor * diffuseIntensity) * specularIntensity * pow(max(0.0f, dot(normalize(-r), normalize(worldPos))), specularPower);
 	// final color
 	// final color
     return float4(saturate((diffuse + ambient + specular) * materialColor), 1.0f);
