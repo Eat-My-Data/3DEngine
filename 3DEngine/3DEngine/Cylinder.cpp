27c89c14437a285d7bf68395b508cc8b33ef47e1
SleepyEthan <Ethan2015D@gmail.com>
2020-11-28 20:35:49 -0600

    cylinder and pyriamids now have more variety for tessalations

--- 3DEngine/3DEngine/Cylinder.cpp
+++ 3DEngine/3DEngine/Cylinder.cpp
@@ -2,46 +2,35 @@
 #include "Prism.h"
 #include "BindableBase.h"
 
-Cylinder::Cylinder(Graphics& gfx, std::mt19937& rng,
+Cylinder::Cylinder( Graphics& gfx,std::mt19937& rng,
 	std::uniform_real_distribution<float>& adist,
 	std::uniform_real_distribution<float>& ddist,
 	std::uniform_real_distribution<float>& odist,
 	std::uniform_real_distribution<float>& rdist,
 	std::uniform_real_distribution<float>& bdist,
-	std::uniform_int_distribution<int>& tdist)
+	std::uniform_int_distribution<int>& tdist )
 	:
-	TestObject(gfx, rng, adist, ddist, odist, rdist)
+	TestObject( gfx,rng,adist,ddist,odist,rdist )
 {
 	namespace dx = DirectX;
 
-	if (!IsStaticInitialized())
+	if( !IsStaticInitialized() )
 	{
-		struct Vertex
-		{
-			dx::XMFLOAT3 pos;
-			dx::XMFLOAT3 n;
-		};
-		auto model = Prism::MakeTesselatedIndependentCapNormals<Vertex>(tdist(rng));
-
-		AddStaticBind(std::make_unique<VertexBuffer>(gfx, model.vertices));
-
-		auto pvs = std::make_unique<VertexShader>(gfx, L"PhongVS.cso");
+		auto pvs = std::make_unique<VertexShader>( gfx,L"PhongVS.cso" );
 		auto pvsbc = pvs->GetBytecode();
-		AddStaticBind(std::move(pvs));
+		AddStaticBind( std::move( pvs ) );
 
-		AddStaticBind(std::make_unique<PixelShader>(gfx, L"IndexedPhongPS.cso"));
-
-		AddStaticIndexBuffer(std::make_unique<IndexBuffer>(gfx, model.indices));
+		AddStaticBind( std::make_unique<PixelShader>( gfx,L"IndexedPhongPS.cso" ) );
 
 		const std::vector<D3D11_INPUT_ELEMENT_DESC> ied =
 		{
 			{ "Position",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0 },
 			{ "Normal",0,DXGI_FORMAT_R32G32B32_FLOAT,0,12,D3D11_INPUT_PER_VERTEX_DATA,0 },
 		};
-		AddStaticBind(std::make_unique<InputLayout>(gfx, ied, pvsbc));
+		AddStaticBind( std::make_unique<InputLayout>( gfx,ied,pvsbc ) );
 
-		AddStaticBind(std::make_unique<Topology>(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
-
+		AddStaticBind( std::make_unique<Topology>( gfx,D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST ) );
+			   
 		struct PSMaterialConstant
 		{
 			dx::XMFLOAT3A colors[6] = {
@@ -55,12 +44,17 @@ Cylinder::Cylinder(Graphics& gfx, std::mt19937& rng,
 			float specularIntensity = 0.6f;
 			float specularPower = 30.0f;
 		} matConst;
-		AddStaticBind(std::make_unique<PixelConstantBuffer<PSMaterialConstant>>(gfx, matConst, 1u));
-	}
-	else
-	{
-		SetIndexFromStatic();
+		AddStaticBind( std::make_unique<PixelConstantBuffer<PSMaterialConstant>>( gfx,matConst,1u ) );
 	}
 
-	AddBind(std::make_unique<TransformCbuf>(gfx, *this));
+	struct Vertex
+	{
+		dx::XMFLOAT3 pos;
+		dx::XMFLOAT3 n;
+	};
+	const auto model = Prism::MakeTesselatedIndependentCapNormals<Vertex>( tdist( rng ) );
+	AddBind( std::make_unique<VertexBuffer>( gfx,model.vertices ) );
+	AddIndexBuffer( std::make_unique<IndexBuffer>( gfx,model.indices ) );
+
+	AddBind( std::make_unique<TransformCbuf>( gfx,*this ) );
 }
--- 3DEngine/3DEngine/Pyramid.cpp
+++ 3DEngine/3DEngine/Pyramid.cpp
@@ -18,38 +18,11 @@ Pyramid::Pyramid(Graphics& gfx, std::mt19937& rng,
 
 	if (!IsStaticInitialized())
 	{
-		struct Vertex
-		{
-			dx::XMFLOAT3 pos;
-			dx::XMFLOAT3 n;
-			std::array<char, 4> color;
-			char padding;
-		};
-		const auto tesselation = tdist( rng );
-		auto model = Cone::MakeTesselatedIndependentFaces<Vertex>( tesselation );
-		// set vertex colors for mesh ( tip red blending to blue base )
-		for (auto& v : model.vertices)
-		{
-			v.color = { (char)10,(char)10,(char)255 };
-		}
-		for ( int i = 0; i < tesselation ; i++ )
-		{
-			model.vertices[i * 3].color = { (char)255,(char)10,(char)10 };
-		}		
-		// squash mesh a bit in the z direction
-		model.Transform(dx::XMMatrixScaling(1.0f, 1.0f, 0.7f));
-		// add normals
-		model.SetNormalsIndependentFlat();
-
-		AddStaticBind(std::make_unique<VertexBuffer>(gfx, model.vertices));
-
 		auto pvs = std::make_unique<VertexShader>(gfx, L"BlendedPhongVS.cso");
 		auto pvsbc = pvs->GetBytecode();
 		AddStaticBind(std::move(pvs));
 
 		AddStaticBind(std::make_unique<PixelShader>(gfx, L"BlendedPhongPS.cso"));
-
-		AddStaticIndexBuffer(std::make_unique<IndexBuffer>(gfx, model.indices));
 
 		const std::vector<D3D11_INPUT_ELEMENT_DESC> ied =
 		{
@@ -60,19 +33,41 @@ Pyramid::Pyramid(Graphics& gfx, std::mt19937& rng,
 		AddStaticBind(std::make_unique<InputLayout>(gfx, ied, pvsbc));
 
 		AddStaticBind(std::make_unique<Topology>(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
-	
+
 		struct PSMaterialConstant
 		{
 			float specularIntensity = 0.6f;
 			float specularPower = 30.0f;
 			float padding[2];
 		} colorConst;
-		AddStaticBind( std::make_unique<PixelConstantBuffer<PSMaterialConstant>>(gfx, colorConst, 1u) );
+		AddStaticBind(std::make_unique<PixelConstantBuffer<PSMaterialConstant>>(gfx, colorConst, 1u));
 	}
-	else
+
+	struct Vertex
 	{
-		SetIndexFromStatic();
+		dx::XMFLOAT3 pos;
+		dx::XMFLOAT3 n;
+		std::array<char, 4> color;
+		char padding;
+	};
+	const auto tesselation = tdist(rng);
+	auto model = Cone::MakeTesselatedIndependentFaces<Vertex>(tesselation);
+	// set vertex colors for mesh (tip red blending to blue base)
+	for (auto& v : model.vertices)
+	{
+		v.color = { (char)10,(char)10,(char)255 };
 	}
+	for (int i = 0; i < tesselation; i++)
+	{
+		model.vertices[i * 3].color = { (char)255,(char)10,(char)10 };
+	}
+	// squash mesh a bit in the z direction
+	model.Transform(dx::XMMatrixScaling(1.0f, 1.0f, 0.7f));
+	// add normals
+	model.SetNormalsIndependentFlat();
+
+	AddBind(std::make_unique<VertexBuffer>(gfx, model.vertices));
+	AddIndexBuffer(std::make_unique<IndexBuffer>(gfx, model.indices));
 
 	AddBind(std::make_unique<TransformCbuf>(gfx, *this));
 }
