45c9e6509ece9052387e26af8c38058a774f2b5a
SleepyEthan <Ethan2015D@gmail.com>
2021-01-08 21:59:07 -0600

    Missed topology in constructor of mesh

--- 3DEngine/3DEngine/Mesh.cpp
+++ 3DEngine/3DEngine/Mesh.cpp
@@ -9,7 +9,7 @@ namespace dx = DirectX;
 // Mesh
 Mesh::Mesh( Graphics& gfx,std::vector<std::shared_ptr<Bind::Bindable>> bindPtrs )
 {
-	AddBind(std::make_shared<Bind::Topology>(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
+	AddBind( Bind::Topology::Resolve(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST) );
 
 	for( auto& pb : bindPtrs )
 	{
