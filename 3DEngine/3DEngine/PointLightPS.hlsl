d635f95d0b31b9b6ee4f7354bd6ce89e4495c115
SleepyEthan <Ethan2015D@gmail.com>
2021-03-31 19:09:01 -0500

    renamed some vairbales

--- 3DEngine/3DEngine/PointLightPS.hlsl
+++ 3DEngine/3DEngine/PointLightPS.hlsl
@@ -26,16 +26,16 @@ float4 main(float4 position : SV_POSITION) : SV_TARGET
     float4 normals = normalTexture.Sample(SampleTypePoint, screenPos);
     float4 specular = specularTexture.Sample(SampleTypePoint, screenPos);
 
-    float depthSample = depthTexture.Sample(SampleTypePoint, screenPos);
+    float depthSample = depthTexture.Sample(SampleTypePoint, screenPos).r;
     float clipX = (screenPos.x - 1.0) * 2.0;
     float clipY = (1 - screenPos.y) * 2.0;
     
     
     float4 worldDepth = float4(clipX, clipY, depthSample, 1.0);
-    float4 viewSpacePosition = mul(worldDepth, mvpMatrix);
-    viewSpacePosition /= viewSpacePosition.w;
+    float4 worldPosition = mul(worldDepth, mvpMatrix);
+    worldPosition /= worldPosition.w;
     
-    float3 vToL = lightPosition - depthSample;
+    float3 vToL = lightPosition.xyz - worldPosition.xyz;
     float len = length(vToL);
     float lightIntensity = saturate(dot(normalize(normals.xyz), normalize(vToL.xyz)));
     
